8a38cbc3f08d1c1eec04aecf0e2185fe57c12655
jhonsbaracaldo <jhonsbaracaldo@gmail.com>
2024-01-29 12:37:20 -0500

    Laboratorio: Inyección de Dependencias en Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ShowService } from './show.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-injection';
+
+  constructor(private showService: ShowService) { }
+
+get items() {
+   return this.showService.items;
 }
+}
+
+
